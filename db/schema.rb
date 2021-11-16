d41c969ca6cc442f4f5179ed4cecefc3df22d353
alicia borchert <16aliciab@gmail.com>
2021-11-16 14:40:53 +0000

    finished landing page with CSS

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_161127) do
+ActiveRecord::Schema.define(version: 2021_11_15_161914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2021_11_15_161127) do
     t.index ["user_id"], name: "index_cakes_on_user_id"
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.bigint "cake_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cake_id"], name: "index_orders_on_cake_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -39,4 +48,6 @@ ActiveRecord::Schema.define(version: 2021_11_15_161127) do
   end
 
   add_foreign_key "cakes", "users"
+  add_foreign_key "orders", "cakes"
+  add_foreign_key "orders", "users"
 end
