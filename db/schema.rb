0a34e85812f36bdc4a02dae728e9e5238f4b6d78
Christopher Hampson <champson@tutanota.com>
2021-11-17 10:58:11 +0000

    Added requests to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_122312) do
+ActiveRecord::Schema.define(version: 2021_11_17_104159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_11_16_122312) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "requests"
     t.index ["cake_id"], name: "index_orders_on_cake_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
