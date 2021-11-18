a90aee6b03cc5b481826078461c52ebbf2549fa1
James Robbs <jamesrobbs01@gmail.com>
2021-11-18 16:08:21 +0000

    pg search and button inline

--- app/models/cake.rb
+++ app/models/cake.rb
@@ -1,4 +1,13 @@
 class Cake < ApplicationRecord
   belongs_to :user
   has_many :orders
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :name, :description ],
+    associated_against: {
+      user: [ :name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
